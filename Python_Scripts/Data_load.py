e73444b39a499b3d42123c91f9718c9b7cc5b165
NtsenoOnGitHub <52259757+NtsenoOnGitHub@users.noreply.github.com>
2026-02-24 15:39:09 +0200

    Update Data_load.py

---
+++ Python_Scripts/Data_load.py
@@ -0,0 +1,38 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+from write_to_log import log
+
+DB_HOST = os.getenv('AWS_DB_HOST')
+DB_PORT = os.getenv('AWS_DB_PORT')
+DB_NAME = os.getenv('AWS_DB_NAME')
+DB_USER = os.getenv('AWS_DB_USER')
+DB_PASWORD = os.getenv('AWS_DB_PASSWORD')
+
+SQL_CREATE_TABLE = """CREATE TABLE IF NOT EXIST insder_transactions (
+                            transaction_date TIMESTAMP,
+                            ticker VARCHAR(50),
+                            executive VARCHAR(50),
+                            executive_title VARCHAR(50),
+                            security_type VARCHAR(50),
+                            acquisition_or_disposal VARCHAR(50),
+                            shares VARCHAR(50),
+                            share_price VARCHAR(50)
+
+)"""
+
+def db_call(query: str):
+    try:
+        db_connection = psycopg2.connect(host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASWORD)
+        cursor = connection.cursor()
+        cursor.execute(query)
+        conection.commit()
+        cursor.close()
+        connection.close()
+        log_message = f"Query executed succfully \n"
+        log(log_message)
+    except Exception as e:
+        log_message = f"Database conection failed: {e}\n"
+        log(log_message)
+
+db_call(SQL_CREATE_TABLE)
